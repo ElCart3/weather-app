b71cc96fc8ca9f99a0ee39bc10b93f03d00efea9
ElCart3 <elliejane.carter@gmail.com>
2021-11-23 11:21:05 +0000

    Transferred CodeSandbox to VS Code

--- src/index.js
+++ src/index.js
@@ -23,31 +23,53 @@ function currentDate() {
 }
 currentDate();
 
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+
+  let tempDisplay = document.querySelector("#current-temp");
+  tempDisplay.innerHTML = temperature;
+  let cityDisplay = document.querySelector("#current-city");
+  cityDisplay.innerHTML = city;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
 
-  let city = document.querySelector("#current-city");
-  city.innerHTML = searchInput.value;
+  let apiKey = "60eb3beb1273b192a3e9365df3418b28";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
 }
+
+function currentTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+
+  let currentTemp = document.querySelector("#current-temp");
+  currentTemp.innerHTML = temperature;
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = city;
+}
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+
+  let apikey = "60eb3beb1273b192a3e9365df3418b28";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`;
+
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+function locate(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentButton = document.querySelector("#current-location");
+currentButton.addEventListener("click", locate);
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
-
-function displayCelsius(event) {
-  event.preventDefault();
-  let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = 16;
-}
-
-let celsiusTemp = document.querySelector("#celsius-link");
-celsiusTemp.addEventListener("click", displayCelsius);
-
-function displayFahrenheit(event) {
-  event.preventDefault();
-  let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = 60;
-}
-
-let fahrenheitTemp = document.querySelector("#fahrenheit-link");
-fahrenheitTemp.addEventListener("click", displayFahrenheit);
